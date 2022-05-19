138b262c25d2a46eedd64541bfbd1d4a0d990d4c
canonwiseman <wisemancanon@gmail.com>
2022-05-19 16:02:25 -0500

    added files for giphy party

--- Giphy_party/ajax-giphy-party/app.js
+++ Giphy_party/ajax-giphy-party/app.js
@@ -1,23 +1,18 @@
 console.log("Let's get this party started!");
 const api_key = 'CaPv4bOi4MdEm0IaJ5y6GZ5r448c1i65';
+const container = document.querySelector("#container");
 
 async function getGif(tag){
-    const gif = await axios.get('https://api.giphy.com/v1/gifs/random', {params: {api_key, tag}});
-    
-    console.log(gif);
+    const gif = await axios.get("https://api.giphy.com/v1/gifs/random", {params: {api_key, tag}});
+
     appendGif(gif.data.data.url);
 }
 
 function appendGif(url){
     const newImg = document.createElement("img");
     newImg.src = url;
-    console.log(newImg);
     container.appendChild(newImg);
-
 }
-
-
-const container = document.querySelector("#container");
 
 const searchBtn = document.querySelector("#search");
 searchBtn.addEventListener("click", function(e){
